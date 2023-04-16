26e5d37cd216aad4ab29b3bf270ada16521805bc
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-04-16 16:44:17 +0100

    updated transaction model

--- project/models/transaction.py
+++ project/models/transaction.py
@@ -1,7 +1,5 @@
 class Transaction:
-    def __init__(self, cost, merchant, tag, user, id = None):
+    def __init__(self, cost, user, id = None):
         self.cost = cost
-        self.merchant = merchant
-        self.tag = tag
         self.user = user
         self.id = id
