5ab2cdac573cd6e0b029e1c8af61ab100ed9af45
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-04-17 19:48:55 +0100

    updated sql

--- project/db/gonzo.sql
+++ project/db/gonzo.sql
@@ -1,12 +1,8 @@
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS merchants;
-DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
-);
 
 CREATE TABLE tags (
     id SERIAL PRIMARY KEY,
@@ -21,9 +17,12 @@ CREATE TABLE merchants (
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     cost INT,    
-    user_id INT NOT NULL REFERENCES users(id),
-    merchant_id INT NOT NULL REFERENCES merchants(id),
-    tag_id INT NOT NULL REFERENCES tags(id)
+    merchant_id INT NOT NULL REFERENCES merchants(id) ON DELETE CASCADE,
+    tag_id INT NOT NULL REFERENCES tags(id) ON DELETE CASCADE
 );
 
-
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    transaction_id INT NOT NULL REFERENCES transactions(id) ON DELETE CASCADE,
+    name VARCHAR(255)
+);
