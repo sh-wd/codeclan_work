fa8d34eba62e9e3c16c40151cfd6e75777cc39ff
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-05-02 09:19:06 +0100

    day 1 commit

---
+++ week_07/day_1/3_todo_list/to_do_list/src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import React, {useState} from 'react'
+
+function App() {
+
+  const [tasks, setTasks] = useState([
+    { name: "Buy groceries", priority: "low"},
+    { name: "Cook dinner", priority: "high" },
+    { name: "Wash dishes", priority: "low" },
+  ]);
+  const [newTask, setNewTask] = useState("");
+  const [newPriority, setNewPriority] = useState("");
+
+
+  const taskNodes = tasks.map((task, index) => {
+    return(
+      <li key={index} className={task.priority}>
+        <span>{ task.name }</span>
+      </li>
+    );
+  });
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handlePriority = (event) => {
+    setNewPriority(event.target.value);
+  };
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks];
+    copyTasks.push({name: newTask, priority: newPriority});
+    setTasks(copyTasks);
+    setNewTask("");
+  };
+
+
+  return (
+    <div className="App">
+
+      <h1>ToDo List</h1>
+      <hr></hr>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor="new-task">Add a new task:</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}/>
+        <input id="high-priority" type="radio" name="priority" value="high" onChange={handlePriority}/>High
+        <input id="low-priority" type="radio" name="priority" value="low" onChange={handlePriority}/>Low
+        <input type="submit" value="Save New Task" />
+      </form>
+      <ul>
+        {taskNodes}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
